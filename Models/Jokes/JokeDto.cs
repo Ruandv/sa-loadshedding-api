cd8fd68bdfd462b7566e673e80ba8c0efe0e5075
Ruan De Villiers <ruan.devilliers@nintex.com>
2023-10-16 23:45:48 +0200

    feat: new controller for humor

---
+++ Models/Jokes/JokeDto.cs
@@ -0,0 +1,12 @@
+﻿using System.Text.Json.Serialization;
+
+namespace Models.Jokes
+{
+    public class JokeDto
+    {
+        [JsonPropertyName("joke")] 
+        public string Joke { get; set; }
+        [JsonPropertyName("answer")]
+        public string Answer { get; set; }
+    }
+}
---
+++ Services/IJokesService.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Threading.Tasks;
+using Models.Jokes;
+
+namespace Services
+{
+    public interface IJokesService
+    {
+        Task<JokeDto> GetJoke();
+    }
+}
